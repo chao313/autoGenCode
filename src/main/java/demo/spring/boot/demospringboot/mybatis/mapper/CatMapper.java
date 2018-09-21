0f068f3ad5fe5896fc98c6cf04d216c72d7287ad
chao <m18888106873@163.com>
2018-09-21 15:52:57 +0800

    init

---
+++ src/main/java/demo/spring/boot/demospringboot/mybatis/mapper/CatMapper.java
@@ -0,0 +1,53 @@
+package demo.spring.boot.demospringboot.mybatis.mapper;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+import demo.spring.boot.demospringboot.mybatis.vo.Cat;
+
+/**
+ * 通过注解来执行sql
+ */
+
+@Mapper
+public interface CatMapper {
+
+    @Select(value = "select * from t_cat where id = #{id}")
+    Cat queryById(@Param(value = "id") Integer idParam);
+
+    @Select(value = "select * from t_cat where name = #{name}")
+    List<Cat> queryByName(@Param(value = "name") String names);
+
+    /**
+     * insert sql 语句可以返回int 这里可以直接接受
+     */
+    @Insert(value = "insert into t_cat (name , age) values(#{name},#{age})")
+    Integer insert(Cat cat);
+
+    /**
+     * 返回插入数据的信息
+     *
+     * 注意,返回的数据就在参数对象里面，不需要返回Integer
+     */
+    @Insert(value = "insert into t_cat (name , age) values(#{name},#{age})")
+    @Options(keyColumn = "id" , useGeneratedKeys = true , keyProperty = "id")
+    void insertReturnId(Cat cat);
+
+
+    /**
+     * 返回更新成功的数量
+     */
+    @Update(value = "update t_cat set name=#{name},age=${age} where id = ${id}")
+    Integer updateById(Cat cat);
+
+    @Delete(value = "delete from t_cat where id = #{id}")
+    Integer deleteById(@Param(value = "id") Integer id);
+}
---
+++ src/main/java/demo/spring/boot/demospringboot/mybatis/service/CatService.java
@@ -0,0 +1,41 @@
+package demo.spring.boot.demospringboot.mybatis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import demo.spring.boot.demospringboot.mybatis.mapper.CatMapper;
+import demo.spring.boot.demospringboot.mybatis.vo.Cat;
+
+@Service
+public class CatService {
+
+    @Autowired
+    private CatMapper mapper;
+
+    public Cat queryById(Integer id) {
+        return mapper.queryById(id);
+    }
+
+    public List<Cat> queryByName(String name) {
+        return mapper.queryByName(name);
+    }
+
+    public Integer insert(Cat cat) {
+        return mapper.insert(cat);
+    }
+
+    public void insertRetrunId(Cat cat) {
+        mapper.insertReturnId(cat);
+    }
+
+    public Integer update(Cat cat) {
+        return mapper.updateById(cat);
+    }
+
+    public Integer delete(Integer id) {
+        return mapper.deleteById(id);
+    }
+
+}
